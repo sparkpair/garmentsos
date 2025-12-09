affd3a98cb151e1aa3f19ea071b5b8b344bd239f
decorgraphicsstudio <hasanraza03165825495@gmail.com>
2025-12-09 11:42:52 +0500

    ..........

--- run.bat
+++ run.bat
@@ -6,4 +6,4 @@ REM --- Start Laravel server in background ---
 start /b npm run dev
 
 REM --- Start Laravel server in background ---
-start /b php artisan serve --host=127.0.0.1 --port=8000
+start /b php artisan serve --port=5425
